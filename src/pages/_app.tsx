bb10cfd402ac9309bb2ae310663c17fdf006cb35
SirYusluv <olanrewajuyusuf379@gmail.com>
2023-03-14 14:23:51 +0100

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import variables from "@/styles/variables.module.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content={variables.colorPrimary} />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/pages/auth/signup/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Signup() {
+  return (
+    <>
+      <Head>
+        <title>Signup</title>
+      </Head>
+    </>
+  );
+}
